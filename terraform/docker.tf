35b65e7c1fe024497701483c45f8ccd25d4395e0
Roozbeh Valavi <valavi.r@gmail.com>
2021-07-08 18:25:26 +1000

    defining server network

--- terraform/docker.tf
+++ terraform/docker.tf
@@ -4,7 +4,20 @@ resource "aws_ecr_repository" "sp_shiny" {
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
-    scan_on_push = false
+    scan_on_push = true
   }  
 }
 
+# build an image locally
+resource "null_resource" "local_r_docker_build" {
+  depends_on = [aws_ecr_repository.r_docker]
+  provisioner "local-exec" {
+    command = <<EOF
+      cd ../docker/R
+      $(aws ecr get-login --registry-ids 364518226878  --no-include-email)
+      docker build -t ${var.project}-r .
+      docker tag ${var.project}-r:latest ${aws_ecr_repository.r_docker.repository_url}:latest
+      docker push ${aws_ecr_repository.r_docker.repository_url}
+    EOF
+  }
+}
---
+++ terraform/network.tf
@@ -0,0 +1,125 @@
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  # tags = {
+  #   Name = "cesar-server"
+  # }
+}
+
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+   route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "cesar-server"
+  }
+}
+
+
+# create a subnet for the sever
+# managing IPs and networking 
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-southeast-2a"
+
+  tags = {
+    Name = "cesar-server"
+  }
+}
+
+
+# reoute table association
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+
+# create security group and ploicies
+resource "aws_security_group" "security" {
+  name        = "allow_connection"
+  description = "Allow SSH, HTTP and Postgres connections"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # cesar ip: 1.136.104.172
+  # home ip:  141.168.90.188
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    # cidr_blocks = ["1.136.104.172","141.168.90.188"]
+  }
+
+  ingress {
+    description = "Shiny"
+    from_port   = 3838
+    to_port     = 3838
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Postgres"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "cesar-server"
+  }
+}
+
+
+# Create a network interface with an ip in the subnet that was created in step 4
+resource "aws_network_interface" "net_interface" {
+  subnet_id       = aws_subnet.subnet.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.security.id]
+
+}
+
+
+# Assign an elastic IP to the network interface created in step 7
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.net_interface.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+
+# print out the public ip of the server
+output "server_public_ip" {
+  value = aws_eip.one.public_ip
+
+}
